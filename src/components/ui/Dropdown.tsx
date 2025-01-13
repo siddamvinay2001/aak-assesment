7c867906ccad475afa226e5c76b065ba11e6dd89
siddamvinay2001 <siddamvinay1368@gmail.com>
2025-01-13 21:45:47 +0530

    feat: UI of signupform

---
+++ src/components/ui/Dropdown.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+
+export interface DropdownItem {
+  id: number;
+  label: string;
+  value: string;
+}
+
+interface DropdownProps {
+  dropdownLabel: string;
+  items: DropdownItem[];
+  buttonStyle?: string;
+  dropdownItemStyle?: string;
+  setUserType: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function Dropdown({
+  dropdownLabel,
+  items,
+  buttonStyle = "",
+  dropdownItemStyle = "",
+  setUserType,
+}: DropdownProps) {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  return (
+    <div className="relative inline-block text-left">
+      <div>
+        <button
+          type="button"
+          className={`${dropdownItemStyle} inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50`}
+          id="menu-button"
+          aria-expanded="true"
+          aria-haspopup="true"
+          onClick={() => setIsOpen((isOpen) => !isOpen)}
+        >
+          {dropdownLabel}
+          <svg
+            className="-mr-1 size-5 text-gray-400"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+            data-slot="icon"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
+              clipRule="evenodd"
+            />
+          </svg>
+        </button>
+      </div>
+      <div
+        className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black/5 focus:outline-none"
+        role="menu"
+        aria-orientation="vertical"
+        aria-labelledby="menu-button"
+        tabIndex="-1"
+      >
+        {isOpen && (
+          <div className="py-1" role="none">
+            {items.map((item, index) => (
+              <button
+                className={`block px-4 py-2 text-sm text-gray-700 ${buttonStyle}`}
+                onClick={() => {
+                  setIsOpen((isOpen) => !isOpen);
+                  setUserType(item.id);
+                }}
+                key={index}
+              >
+                {item.label}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,24 +1,170 @@
+import { useState } from "react";
+import Dropdown from "../components/ui/Dropdown";
+import { CountryDropdown } from "react-country-region-selector";
+
+const items = [
+  {
+    id: 0,
+    label: "Researcher",
+    value: "researcher",
+  },
+  {
+    id: 1,
+    label: "Investor",
+    value: "investor",
+  },
+  {
+    id: 2,
+    label: "Institution Staff",
+    value: "institution_staff",
+  },
+  {
+    id: 3,
+    label: "Service Provider",
+    value: "service_provider",
+  },
+];
+
 export default function Signup() {
   const handleFormSubmit: () => void = () => {
     console.log("Form is submitted");
   };
 
+  const [userType, setUserType] = useState<number>(0);
+
   return (
-    <div className="w-screen h-screen bg-slate-300 flex justify-center items-center">
-      <div className="bg-white shadow-md rounded-md p-7">
-        <form onSubmit={handleFormSubmit}>
-          <div className="">Heading and side heading</div>
-          <div className="">Firstname</div>
-          <div>LastName</div>
-          <div>Username</div>
-          <div>Country</div>
-          <div>Email</div>
-          <div>PAssword</div>
-          <div> Confirm Password</div>
-          <div>Agree</div>
-          <div>Button</div>
-        </form>
+    <>
+      <div className="bg-slate-300 flex min-h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-blue-700">
+            AAK
+          </h2>
+          <h2 className="mt-4 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
+            Sigup your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form action="#" method="POST" className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Tell us what type of account you'd be opening.
+              </label>
+              <div className="mt-2">
+                <Dropdown />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="firstName"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                First Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="fistName"
+                  name="firstName"
+                  type="text"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="lastName"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Last Name
+              </label>
+              <div className="mt-2">
+                <input
+                  id="lastName"
+                  name="lastName"
+                  type="text"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  required
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="createPassword"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Create Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="createPassword"
+                  name="createPassword"
+                  type="password"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="confirmPassword"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Confirm Password
+              </label>
+              <div className="mt-2">
+                <input
+                  id="confirmPassword"
+                  name="confirmPassword"
+                  type="password"
+                  required
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="country"
+                className="block text-sm/6 font-medium text-gray-900"
+              >
+                Country
+              </label>
+              <div className="">
+                <CountryDropdown value={""} onChange={() => {}} />
+              </div>
+            </div>
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm/6 font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
